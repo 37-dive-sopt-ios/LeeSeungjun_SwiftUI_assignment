52de152d1c0c5719560cf1b77745e243770adf19
Rudy-009 <rofxnaos@gmail.com>
2025-12-02 22:48:10 +0900

    [feat] search bar 구현
    overlay 사용하기

--- SOPT-SwiftUI-Assignment/ContentView.swift
+++ SOPT-SwiftUI-Assignment/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationBar()
-            .padding([.leading, .trailing], 16)
+            .padding([.horizontal], 16)
+        SearchBar()
+            .padding([.horizontal], 16)
+            .padding([.top], 4)
+        EventPromotionView()
+            .padding([.horizontal], 16)
+            .padding([.top], 24)
         Spacer()
     }
 }
@@ -46,6 +52,37 @@ struct NavigationBar: View {
             }
         }
         .frame(height: 24)
+    }
+}
+
+struct SearchBar: View {
+    var body: some View {
+        HStack {
+            TextField("찾아라! 맛있는 음식과 맛집", text: .constant(""))
+                .padding([.horizontal], 20)
+                .padding([.vertical], 10)
+                .font(Font.pretendard(.body_r_14))
+                .foregroundColor(.black)
+            
+            Image(.search)
+                .padding([.trailing], 17)
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.black, lineWidth: 1))
+        .frame(height: 40)
+    }
+}
+
+struct EventPromotionView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            Spacer()
+        }
     }
 }
 
