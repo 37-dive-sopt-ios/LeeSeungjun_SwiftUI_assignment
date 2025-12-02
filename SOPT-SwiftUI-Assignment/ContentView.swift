15085d13b19439dd571c28192ef10df34f6d1bd4
Rudy-009 <rofxnaos@gmail.com>
2025-12-02 21:23:29 +0900

    [chore] 프로젝트 생성 및 에셋 추가

---
+++ SOPT-SwiftUI-Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOPT-SwiftUI-Assignment
+//
+//  Created by 이승준 on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
