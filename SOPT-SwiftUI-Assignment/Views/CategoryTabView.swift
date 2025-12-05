d5ead1714390328894a4f70b82734ebfc622444d
Rudy-009 <rofxnaos@gmail.com>
2025-12-05 22:16:02 +0900

    [feat] Tab을 누를 때, 검은색 텍스트로 변경

--- SOPT-SwiftUI-Assignment/Views/CategoryTabView.swift
+++ SOPT-SwiftUI-Assignment/Views/CategoryTabView.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 
 struct CategoryTabView: View {
+    
+    @State var selectedIndex: Int = 0
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack {
                 ForEach(CategoryData.data.enumerated(), id: \.offset) { index, data in
                     Button {
-                        
+                        selectedIndex = index
                     } label: {
-                        Text("\(data.name)")
-                            .font(Font.pretendard(.bold_18))
-                            .tint(.gray)
-                            .padding([.horizontal], 7)
+                        TabCell(
+                            selectedIndex: $selectedIndex,
+                            index: index,
+                            name: data.name)
                     }
                 }
             }
@@ -29,4 +32,22 @@ struct CategoryTabView: View {
         .cornerRadius(10)
         .shadow(color: .baeminMint300, radius: 4, x: 0, y: -6)
     }
+}
+
+struct TabCell: View {
+    
+    @Binding var selectedIndex: Int
+    var index: Int
+    var name: String
+    
+    var body: some View {
+        Text(name)
+            .font(Font.pretendard(.bold_18))
+            .tint(selectedIndex == index ? .baeminBlack : .baeminGray300)
+            .padding([.horizontal], 7)
+    }
+}
+
+#Preview {
+    ContentView()
 }
