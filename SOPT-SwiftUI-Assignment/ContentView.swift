dcd617f769ca657905610914592b89079621ebff
Rudy-009 <rofxnaos@gmail.com>
2025-12-09 15:09:05 +0900

    [feat] TabView 추가

--- SOPT-SwiftUI-Assignment/ContentView.swift
+++ SOPT-SwiftUI-Assignment/ContentView.swift
@@ -12,26 +12,77 @@ struct ContentView: View {
     @State var selectedIndex: Int = 0
     
     var body: some View {
-        ScrollView {
-            NavigationBar()
-                .padding([.horizontal], 16)
-            SearchBar()
-                .padding([.horizontal], 16)
-                .padding([.top], 4)
-            EventPromotionView()
-                .padding([.top], 24)
-            CategoryTabView(selectedIndex: $selectedIndex)
-                .padding([.top], -30)
-            CategoryScrollView(selectedIndex: $selectedIndex)
-                .padding([.top], -7)
-            MartketcrollView()
-                .padding([.top], 5)
-            BannerScrollView()
-                .padding([.top], 5)
-            RankScrollView()
-            Spacer()
+        TabView {
+            ScrollView {
+                NavigationBar()
+                    .padding([.horizontal], 16)
+                SearchBar()
+                    .padding([.horizontal], 16)
+                    .padding([.top], 4)
+                EventPromotionView()
+                    .padding([.top], 24)
+                CategoryTabView(selectedIndex: $selectedIndex)
+                    .padding([.top], -30)
+                CategoryScrollView(selectedIndex: $selectedIndex)
+                    .padding([.top], -7)
+                MartketcrollView()
+                    .padding([.top], 5)
+                BannerScrollView()
+                    .padding([.top], 5)
+                RankScrollView()
+                Spacer()
+            }
+            .background(.baeminBackgroundWhite)
+            .tabItem {
+                Image(.homeSelected)
+                Text("홈")
+                Color(.black)
+            }
+            
+            VStack {
+                Text("장보기·쇼핑")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.yellow)
+            .tabItem {
+                Image(.shop)
+                Text("장보기·쇼핑")
+                Color(.black)
+            }
+            
+            VStack {
+                Text("찜")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.orange)
+            .tabItem {
+                Image(.heart)
+                Text("찜")
+                Color(.black)
+            }
+            
+            VStack {
+                Text("주문내역")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.green)
+            .tabItem {
+                Image(.list)
+                Text("주문내역")
+                Color(.black)
+            }
+            
+            VStack {
+                Text("마이배민")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.baeminBlue500)
+            .tabItem {
+                Image(.myPage)
+                Text("마이배민")
+                Color(.black)
+            }
         }
-        .background(.baeminBackgroundWhite)
     }
 }
 
