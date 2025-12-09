8079dbd8f926fc9be5c46537606464a8e67744fd
Rudy-009 <rofxnaos@gmail.com>
2025-12-09 15:44:53 +0900

    [feat] 선택된 카테고리에 따라 centered 되게 기능 추가

--- SOPT-SwiftUI-Assignment/Views/Home/CategoryTabView.swift
+++ SOPT-SwiftUI-Assignment/Views/Home/CategoryTabView.swift
@@ -13,27 +13,38 @@ struct CategoryTabView: View {
     @Namespace private var animationNamespace
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            LazyHStack {
-                ForEach(CategoryData.data.enumerated(), id: \.offset) { index, data in
-                    Button {
-                        withAnimation(.easeInOut(duration: 0.3)) {
-                            selectedIndex = index
+        ScrollViewReader { proxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach(CategoryData.data.enumerated(), id: \.offset) { index, data in
+                        Button {
+                            withAnimation(.easeInOut(duration: 0.3)) {
+                                selectedIndex = index
+                            }
+                        } label: {
+                            TabCell(
+                                selectedIndex: $selectedIndex,
+                                index: index,
+                                name: data.name,
+                                namespace: animationNamespace)
                         }
-                    } label: {
-                        TabCell(
-                            selectedIndex: $selectedIndex,
-                            index: index,
-                            name: data.name,
-                            namespace: animationNamespace)
                     }
                 }
+                .padding([.horizontal], 16)
             }
-            .padding([.horizontal], 16)
+            .onChange(of: selectedIndex) {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    proxy.scrollTo(selectedIndex, anchor: .center)
+                }
+            }
+            .frame(height: 48)
+            .background(.white)
+            .cornerRadius(10, corners: [.topLeft, .topRight])
+            .shadow(color: .baeminMint300, radius: 4, x: 0, y: -6)
         }
-        .frame(height: 48)
-        .background(.white)
-        .cornerRadius(10, corners: [.topLeft, .topRight])
-        .shadow(color: .baeminMint300, radius: 4, x: 0, y: -6)
     }
+}
+
+#Preview {
+    ContentView()
 }
